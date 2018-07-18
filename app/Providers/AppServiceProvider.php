4e604198a534ba255c3602067fc37d7c09b414d0
Amin Ghaninia <41255155+ghaninia@users.noreply.github.com>
2018-07-19 00:41:56 +0430

    آپدیت appServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+		if( date("H") == 24 )
+			\Artisan::call("fresh:plan");
+			
         Schema::defaultStringLength(191) ;
     }
 
